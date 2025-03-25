11b5425b642551384d6f1751649195f05b11dba3
jevg2003 <jevgdg@gmail.com>
2025-03-24 19:09:32 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://jevg2003.github.io/orderlys1.0/', // 🔹 Cambia esto
+  base: '/orderlys1.0/', // 🔹 Cambia esto también
+  output: 'static' // 🚀 Astro necesita esto para GitHub Pages
 });
