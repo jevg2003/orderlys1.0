529d390d23ac13bb5d2dd937027b7b045bf57172
jevg2003 <jevgdg@gmail.com>
2025-03-24 21:21:05 -0500

    update changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './dist',
   vite: {
     plugins: [tailwindcss()]
   },
   site: 'https://jevg2003.github.io',
-  base: '/orderlys1.0/',
+  base: '/orderlys1.0/'
 });
